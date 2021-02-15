ea9762cb5ad33582d7ad4d538b23b4e8eb921fae
saralitwak <sara.litwak@twipemobile.com>
2021-02-15 11:35:20 +0100

    Updates

---
+++ precache-manifest.e79e1f3ed55271d421e46848e6c10649.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c803dc7e3a3965ab18c7c0b77f72011f",
+    "url": "/react-redux-list/index.html"
+  },
+  {
+    "revision": "8e747c858c1a8e57ab9b",
+    "url": "/react-redux-list/static/js/2.51140073.chunk.js"
+  },
+  {
+    "revision": "4530570eadd2e36a2619",
+    "url": "/react-redux-list/static/js/main.611bafa0.chunk.js"
+  },
+  {
+    "revision": "d5efb71a3284dbe16b16",
+    "url": "/react-redux-list/static/js/runtime-main.4cbfa75a.js"
+  }
+]);
